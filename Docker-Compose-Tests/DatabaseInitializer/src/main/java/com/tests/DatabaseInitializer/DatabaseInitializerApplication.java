d460fdfcdb632cf4e12812b2ff4de92b5e940d23
DJBrainiac09 <japas102@hhu.de>
2019-02-11 13:59:18 +0100

    Adding new directories for testing Docker-Compose.

---
+++ Docker-Compose-Tests/DatabaseInitializer/src/main/java/com/tests/DatabaseInitializer/DatabaseInitializerApplication.java
@@ -0,0 +1,14 @@
+package com.tests.DatabaseInitializer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseInitializerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseInitializerApplication.class, args);
+	}
+
+}
+
---
+++ Docker-Compose-Tests/DatabaseTest/src/main/java/com/tests/DatabaseTest/DatabaseTestApplication.java
@@ -0,0 +1,14 @@
+package com.tests.DatabaseTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseTestApplication.class, args);
+	}
+
+}
+
