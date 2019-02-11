d846b70a228c9a0ec27c6c89edaa3a10e4f48e4b
Oliver Nern <olivernern@gmail.com>
2019-02-11 15:14:49 +0100

    Ordnerstruktur hoffentlich repariert

---
+++ Benutzer/src/main/java/de/ProPra/Benutzer/BenutzerApplication.java
@@ -0,0 +1,14 @@
+package de.ProPra.Benutzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenutzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenutzerApplication.class, args);
+	}
+
+}
+
---
+++ Benutzer/src/test/java/de/ProPra/Benutzer/VerleihApplicationTests.java
@@ -0,0 +1,17 @@
+package de.ProPra.Benutzer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VerleihApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
