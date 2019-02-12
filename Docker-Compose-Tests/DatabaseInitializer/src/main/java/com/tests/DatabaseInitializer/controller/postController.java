62d43610231444d1bb9ea0b9119c60613d61cd45
DoktorFleisch <daniel.warkus@hhu.de>
2019-02-12 14:31:28 +0100

    changed controller and html

--- Docker-Compose-Tests/DatabaseInitializer/src/main/java/com/tests/DatabaseInitializer/controller/postController.java
+++ Docker-Compose-Tests/DatabaseInitializer/src/main/java/com/tests/DatabaseInitializer/controller/postController.java
@@ -4,6 +4,7 @@ import com.tests.DatabaseInitializer.dataaccess.PersonRepository;
 import com.tests.DatabaseInitializer.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,8 +21,8 @@ public class postController {
     }
 
     @PostMapping("/add")
-    public String postObjectInDatabase(@ModelAttribute("person") Person person) {
-        personRepository.save(person);
+    public String postObjectInDatabase(Model model, String name) {
+        personRepository.save(new Person(name));
         return "addNewPerson";
     }
 }
