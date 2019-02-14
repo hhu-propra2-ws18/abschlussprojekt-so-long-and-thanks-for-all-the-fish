46cb66d7d67065c0ab3ea88fd2c81a56d7a809f5
xpanded <jeffnosbusch97@gmail.com>
2019-02-14 14:22:00 +0100

    WIr haben Thymeleaf auf user.attribute gemacht damit wir im Modell nicht alle module einzeln übergeben müssen Franz und Jeff

--- Benutzer/src/test/java/de/ProPra/Benutzer/VerleihApplicationTests.java
+++ Benutzer/src/test/java/de/ProPra/Benutzer/VerleihApplicationTests.java
@@ -1,17 +1,44 @@
 package de.ProPra.Benutzer;
 
+import de.ProPra.Benutzer.Controller.UserController;
+import de.ProPra.Benutzer.model.User;
+import de.ProPra.Benutzer.service.DatabaseInitializer;
+import de.ProPra.Benutzer.service.UserRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class VerleihApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void testIsValidEmail() {
+        UserController uc = new UserController();
+        assertEquals(false, uc.isValidEmailAddress("testil.com"));
+        assertEquals(true, uc.isValidEmailAddress("jeff@hhu.de"));
+    }
+
+    @Test
+    public void testIsValidUsername() {
+        UserController uc = new UserController();
+        assertEquals(false, uc.isValidUsername("franz hoffmann"));
+        assertEquals(true, uc.isValidUsername("franz_hoffmann"));
+        assertEquals(false, uc.isValidUsername(""));
+    }
+
 
 }
 
