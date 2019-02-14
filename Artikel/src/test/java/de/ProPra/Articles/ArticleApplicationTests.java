ecf2afb7411808f82fab4f8df3a98727070742fe
David Huseyin <dahus101@uni-duesseldorf.de>
2019-02-14 15:14:35 +0100

    update .travis.yml and build.gradle for sonarcloud support

--- Artikel/src/test/java/de/ProPra/Articles/ArticleApplicationTests.java
+++ Artikel/src/test/java/de/ProPra/Articles/ArticleApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class VerleihApplicationTests {
+public class ArticleApplicationTests {
 
 	@Test
 	public void contextLoads() {
