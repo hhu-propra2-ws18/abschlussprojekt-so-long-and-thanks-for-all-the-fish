15ab850e248e6912de7679b6dfe8413513d11691
DJBrainiac09 <japas102@hhu.de>
2019-02-19 14:29:20 +0100

    morning ..

--- Login-service/Loginservice/testservice/src/main/java/com/jacquespasquier/testservice/web/PageController.java
+++ Login-service/Loginservice/testservice/src/main/java/com/jacquespasquier/testservice/web/PageController.java
@@ -1,6 +1,6 @@
 package com.jacquespasquier.testservice.web;
 
-import org.springframework.security.access.annotation.Secured;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 @Controller
