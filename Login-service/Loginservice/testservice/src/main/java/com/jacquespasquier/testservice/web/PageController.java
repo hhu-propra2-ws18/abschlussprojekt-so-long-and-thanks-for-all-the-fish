5ba0882813de181b159b6ad660790cbe923bd985
DJBrainiac09 <japas102@hhu.de>
2019-02-19 14:19:26 +0100

    from this morning -.-

--- Login-service/Loginservice/testservice/src/main/java/com/jacquespasquier/testservice/web/PageController.java
+++ Login-service/Loginservice/testservice/src/main/java/com/jacquespasquier/testservice/web/PageController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PageController {
-    @Secured("ROLE_ADMIN")
+
     @GetMapping("/")
     public String loadPage() {
         return "testpage";
