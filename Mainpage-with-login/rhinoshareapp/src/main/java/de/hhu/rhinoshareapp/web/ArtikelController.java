ee1b9435faf9e1cc20927bdad8f4ddb20ed30677
DoktorFleisch <daniel.warkus@hhu.de>
2019-02-20 14:10:36 +0100

    added my artikelwebseite from mainpagedevbranch, a controllter

--- Mainpage-with-login/rhinoshareapp/src/main/java/de/hhu/rhinoshareapp/web/ArtikelController.java
+++ Mainpage-with-login/rhinoshareapp/src/main/java/de/hhu/rhinoshareapp/web/ArtikelController.java
@@ -1,4 +1,11 @@
 package de.hhu.rhinoshareapp.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class ArtikelController {
+
+    @GetMapping
+    public String getArtikelPage() {
+        return "artikel";
+    }
 }
