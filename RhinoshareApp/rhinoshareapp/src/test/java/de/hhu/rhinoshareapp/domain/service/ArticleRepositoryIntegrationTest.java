f24001f71df1e28cee7f4dd3f3850bfb00200c4d
Oliver Nern <olivernern@gmail.com>
2019-02-21 16:22:16 +0100

    merged Lending Tests into rhinoapp

--- RhinoshareApp/rhinoshareapp/src/test/java/de/hhu/rhinoshareapp/domain/service/ArticleRepositoryIntegrationTest.java
+++ RhinoshareApp/rhinoshareapp/src/test/java/de/hhu/rhinoshareapp/domain/service/ArticleRepositoryIntegrationTest.java
@@ -1,5 +1,6 @@
 package de.hhu.rhinoshareapp.domain.service;
 
+import de.hhu.rhinoshareapp.*;
 import de.hhu.rhinoshareapp.domain.model.Article;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,6 +14,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
