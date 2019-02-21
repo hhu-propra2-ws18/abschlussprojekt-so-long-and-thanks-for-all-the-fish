d201a49a734d2b8acccc89ff20ec0792218eb81c
Robnarok <Rokra102@hhu.de>
2019-02-21 11:22:00 +0100

    fixes bug with Images

--- RhinoshareApp/rhinoshareapp/src/main/java/de/hhu/rhinoshareapp/controller/ImageController.java
+++ RhinoshareApp/rhinoshareapp/src/main/java/de/hhu/rhinoshareapp/controller/ImageController.java
@@ -1,7 +1,7 @@
-package de.ProPra.Articles.infrastructure.web;
+package de.hhu.rhinoshareapp.controller;
 
-import de.ProPra.Articles.domain.model.Image;
-import de.ProPra.Articles.domain.service.ImageRepository;
+import de.hhu.rhinoshareapp.domain.model.Image;
+import de.hhu.rhinoshareapp.domain.service.ImageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
