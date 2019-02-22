a92190703b85c001015c7f3c97a5457701ecee82
Robnarok <Rokra102@hhu.de>
2019-02-22 13:12:23 +0100

    Removed the Trahs

--- Conflict/src/main/java/de/hhu/ProPra/conflict/service/Repositorys/ArticleRepository.java
+++
@@ -1,7 +0,0 @@
-package de.hhu.ProPra.conflict.service.Repositorys;
-
-import de.hhu.ProPra.conflict.model.Article;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ArticleRepository extends CrudRepository<Article, Long> {
-}
