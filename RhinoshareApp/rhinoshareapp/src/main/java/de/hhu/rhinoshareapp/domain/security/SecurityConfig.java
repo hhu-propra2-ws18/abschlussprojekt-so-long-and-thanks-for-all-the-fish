04ca62747ba559f929ac96e1568266ab0ad5c0c4
David <davemithut@googlemail.com>
2019-02-28 13:48:21 +0100

    add redirection after login feature

--- RhinoshareApp/rhinoshareapp/src/main/java/de/hhu/rhinoshareapp/domain/security/SecurityConfig.java
+++ RhinoshareApp/rhinoshareapp/src/main/java/de/hhu/rhinoshareapp/domain/security/SecurityConfig.java
@@ -1,25 +1,29 @@
 package de.hhu.rhinoshareapp.domain.security;
 
+import de.hhu.rhinoshareapp.domain.service.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    UserDetailsService userDetailsService;
+    UserRepository userRepository;
 
     @Bean
     public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler myUrlAuthenticationHandler(){
+        return new MyUrlAuthenticationHandler();
     }
 
     @Override
@@ -35,6 +39,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                 .loginPage("/login")
+                .successHandler(myUrlAuthenticationHandler())
                 .permitAll()
                 .and()
                 .logout().logoutSuccessUrl("/login")
